5461b04c508d65e568b56afe28f0e12af0c91ba5
evelynhelena <evelynhelena781@gmail.com>
2022-01-07 19:12:12 -0300

    Fazendo autenticação com o github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 import "../styles/global.scss";
+import { SessionProvider as NestAuthProvider } from "next-auth/react";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    <NestAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
+    </NestAuthProvider>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,49 @@
 //import styles from "../styles/home.module.scss";
+import { GetStaticProps } from 'next'
 import Head from 'next/head';
-export default function Home() {
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../Services/stripe';
+import styles from './home.module.scss';
+
+interface HomeProps{
+  product:{
+    priceId: string;
+    amount: number;
+  }
+} 
+
+export default function Home({product}:HomeProps) {
   return (
     <>
       <Head>
-        <title> Inicio | ignews</title>
+        <title> Home | ignews</title>
       </Head>
-      <h1>
-        Ola <span>mundo</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>Get acess to all the publications <br /> <span>or {product.amount} month</span></p>
+
+          <SubscribeButton priceId={product.priceId}/>
+        </section>
+        <img src="images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KFLKTLiy1zMR4IDpMbB0b0a')
+
+  const product = {
+    priceId: price.id,
+    amount: Intl.NumberFormat('es-US',{style:'currency',currency:'USD'}).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 Hours
+  }
 }
