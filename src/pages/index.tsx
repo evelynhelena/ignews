5d8673fd34cc085dab4d7232fb7c8d72619b7396
evelynhelena <evelynhelena781@gmail.com>
2022-01-06 21:14:57 -0300

    iniciando

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+//import styles from "../styles/home.module.scss";
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title> Inicio | ignews</title>
+      </Head>
+      <h1>
+        Ola <span>mundo</span>
+      </h1>
+    </>
+  )
+}
