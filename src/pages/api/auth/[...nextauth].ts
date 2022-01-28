49df4188a396a169909cd5005827abc0bd6cb2e9
evelynhelena <evelynhelena781@gmail.com>
2022-01-28 18:56:20 -0300

    criando metodo de pagamento

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
+import { query as q } from 'faunadb';
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-
+import { fauna } from "../../../Services/fauna";
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -9,4 +10,36 @@ export default NextAuth({
       authorization: { params: { scope: "read:user" } },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'), 
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
